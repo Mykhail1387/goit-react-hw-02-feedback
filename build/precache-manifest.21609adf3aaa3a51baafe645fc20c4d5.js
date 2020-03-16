8fdcef7803b8622d83f3b68401e5318017bf2987
Mykhail1387 <melnychukmykhailo87@gmail.com>
2020-03-16 13:11:00 +0200

    my second homework with build

---
+++ build/precache-manifest.21609adf3aaa3a51baafe645fc20c4d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4718372f3ddaebc0883294f6f0b23f69",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "dae76ab834ad113e9330",
+    "url": "/goit-react-hw-02-feedback/static/css/main.5236fae9.chunk.css"
+  },
+  {
+    "revision": "efde0be0a96d64ce4aa3",
+    "url": "/goit-react-hw-02-feedback/static/js/2.54f0d933.chunk.js"
+  },
+  {
+    "revision": "dae76ab834ad113e9330",
+    "url": "/goit-react-hw-02-feedback/static/js/main.aa316929.chunk.js"
+  },
+  {
+    "revision": "8831d0a89062d33b1e08",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.185b178b.js"
+  }
+]);
